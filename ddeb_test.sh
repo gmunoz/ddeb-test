c6958c6307eca7af5286f1bacd13d3798202c586
Gabriel Muñoz <gmunoz@netflix.com>
2023-04-14 10:57:47 -0700

    Allow variation for bionic
    Add ping data for ddebs.ubuntu.com

--- ddeb_test.sh
+++ ddeb_test.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 apt-get update
 apt-get install -y --no-install-recommends lsb-release
+
+distro=$(lsb_release -cs)
+readonly distro
+
+pkg=zlib1g-dbgsym
+if [[ $distro == bionic ]]; then
+  pkg=zlib1g-dbg
+fi
+readonly pkg
 
 echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
 deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
@@ -12,6 +21,9 @@ tee -a /etc/apt/sources.list.d/ddebs.list
 
 apt-get install -y ubuntu-dbgsym-keyring
 
+ping -c 2 ddebs.ubuntu.com
+
 # Observe output for HTTP 404s/503s and/or warnings about index download
 # failures due to hash mismatches.
 apt-get update
+apt-get install -y --no-install-recommends "${pkg}"
